f1ab8cefddb768a9b1391544b8d60d994c2aff4d
Wade <wadephilander7@gmail.com>
2020-09-16 16:36:17 +0200

    my initial commit

---
+++ hello world/Hello.py
@@ -0,0 +1,16 @@
+"""
+print("YAAAAAAAAH MAN")
+
+print("I stay in wherever")
+#
+firstname = input("what is your name")
+print("My new name is ", firstname)
+"""
+# =======================AREA OF A CIRCLE======================================
+
+r = int(input("Radius:"))
+pi=3.14
+
+circle = (pi * r * r)
+print(circle)
+
